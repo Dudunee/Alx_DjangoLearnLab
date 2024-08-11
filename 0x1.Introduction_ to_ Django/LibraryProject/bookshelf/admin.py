94f56273e596b2b58ed10e5faf76a51f288e7f50
Yahya Bakhouch <yahyabakhouchh@gmail.com>
2024-08-11 13:19:00 +0100

    add admin interface

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    search_fields = ('title', 'author', 'publication_year')
-    search__fields = ('title', 'author')
+    list_filter = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
 
 admin.site.register(Book)
 # Register your models here.
