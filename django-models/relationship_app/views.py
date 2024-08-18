57a03aff1fab4e20bc81ab151100e5dcaabb7f40
Yahya Bakhouch <yahyabakhouchh@gmail.com>
2024-08-18 20:34:21 +0100

    add authentication

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -14,4 +14,25 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home') 
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
 # Create your views here.
