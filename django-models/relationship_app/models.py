1d077ed9ae31a427ddb4983f1066ed28ad45f865
Yahya Bakhouch <yahyabakhouchh@gmail.com>
2024-08-18 21:05:32 +0100

    add custom permissions

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -35,4 +35,15 @@ class UserProfile(models.Model):
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=15, choices=ROLE_CHOICES)
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
 # Create your models here.
--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, admin_view, librarian_view, member_view
+from .views import list_books, LibraryDetailView, admin_view, librarian_view, member_view, add_book, edit_book, delete_book
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -10,4 +10,7 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
+    path('add/', add_book, name='add_book'),
+    path('edit/edit_book/', edit_book, name='edit_book'),
+    path('delete/delete_book>/', delete_book, name='delete_book'),
 ]
