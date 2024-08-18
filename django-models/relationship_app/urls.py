b0d82a783a13c1b6da408ece3c21c0f25045a15b
Yahya Bakhouch <yahyabakhouchh@gmail.com>
2024-08-18 20:38:04 +0100

    add authentication

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,7 +4,7 @@ from .views import list_books, LibraryDetailView
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/details/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
+    path('login/', LoginView.as_view(), template_name='login'),
+    path('logout/', LogoutView.as_view(), template_name='logout'),
+    path('register/', views.register, name='register'),
 ]
