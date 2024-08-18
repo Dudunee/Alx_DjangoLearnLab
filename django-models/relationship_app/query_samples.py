58977a10f0b996f87b9278717b2b79988e22f357
Yahya Bakhouch <yahyabakhouchh@gmail.com>
2024-08-18 19:21:43 +0100

    add models and query

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,6 +1,6 @@
 
 Book.objects.filter(author__name='Author Name')
 
-["Library.objects.get(name=library Name)"]
+["Library.objects.get(name=library Name)",".books.all()"]
 
 Librarian.objects.get(library__name='Library Name')
