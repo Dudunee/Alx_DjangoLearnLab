8afd16d17c965ad3cdc5fc0c296e98c673d88b90
Yahya Bakhouch <yahyabakhouchh@gmail.com>
2024-08-25 23:14:51 +0100

    project week 11

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import MyModel
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_view(request, pk):
+    instance = get_object_or_404(MyModel, pk=pk)
+    if request.method == 'POST':
+        ...
+    return render(request, 'edit_template.html', {'instance': instance})
 
 def index(request):
     return HttpResponse("Welcome to my book store.")
