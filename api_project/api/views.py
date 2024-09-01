d6e624acc7140f4ea1cb7755062275f19e788b19
Yahya Bakhouch <yahyabakhouchh@gmail.com>
2024-09-01 19:51:13 +0100

    intro to api

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -7,7 +7,7 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookViewSet(viewsets.ModelViewset):
+class BookViewSet(viewsets.ModelViewSet):
     
     def list(self, request):
         queryset = Book.objects.all()
