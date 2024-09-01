a146170c775f6aab3ec616f8f2819f9d01851903
Yahya Bakhouch <yahyabakhouchh@gmail.com>
2024-09-01 19:18:18 +0100

    create serializers and urls

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,6 @@ from .models import Book
 from .serializers import BookSerializer
 
 
-class BookList(generics.ListAPIView):
+class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
