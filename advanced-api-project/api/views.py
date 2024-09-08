d215bfe047e35c88733b3a364629eb6bf7892604
Yahya Bakhouch <yahyabakhouchh@gmail.com>
2024-09-08 16:01:57 +0100

    advanced_api_project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,8 +3,8 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter, OrderingFilter, rest_framework
- 
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
