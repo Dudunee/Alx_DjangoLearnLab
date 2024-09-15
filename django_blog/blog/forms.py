45b77c138efb17896cc3b2f9e538d92d7e5787ad
Yahya Bakhouch <yahyabakhouchh@gmail.com>
2024-09-15 19:43:30 +0100

    add django_blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm
-from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  
-            messages.success(request, f'Account created successfully!')
-            return redirect('profile') 
+            login(request, user)
+            messages.success(request, 'Your account has been created!')
+            return redirect('profile')
     else:
         form = UserRegisterForm()
     return render(request, 'blog/register.html', {'form': form})
@@ -22,7 +22,7 @@ def profile(request):
         form = UserUpdateForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            messages.success(request, f'Your account has been updated!')
+            messages.success(request, 'Your profile has been updated!')
             return redirect('profile')
     else:
         form = UserUpdateForm(instance=request.user)
