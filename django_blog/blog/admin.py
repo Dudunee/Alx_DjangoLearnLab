908069a9fb028a086326f71ef319b000c263b5a2
Yahya Bakhouch <yahyabakhouchh@gmail.com>
2024-09-15 18:11:18 +0100

    add django_blog

---
+++ django_blog/blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_date', 'author')
+    list_filter = ('author', 'published_date')
+    search_fields = ('title', 'content')
