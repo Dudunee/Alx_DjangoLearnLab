9092050a1d1d4be7bd14b5055bb495a242e13b91
Yahya Bakhouch <yahyabakhouchh@gmail.com>
2024-09-22 20:38:10 +0100

    social media api

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,11 +1,13 @@
 # accounts/views.py
 
-from rest_framework import generics
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from .models import CustomUser
 from .serializers import UserSerializer, UserRegistrationSerializer
+from django.contrib.auth import get_user_model
+from rest_framework.decorators import api_view
 
 class UserRegistrationView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
@@ -24,3 +26,18 @@ class UserLoginView(ObtainAuthToken):
 class UserProfileView(generics.RetrieveUpdateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+
+
+User = get_user_model()
+
+@api_view(['POST'])
+def follow_user(request, user_id):
+    user_to_follow = User.objects.get(id=user_id)
+    request.user.following.add(user_to_follow)
+    return Response({"message": "Following user."})
+
+@api_view(['POST'])
+def unfollow_user(request, user_id):
+    user_to_unfollow = User.objects.get(id=user_id)
+    request.user.following.remove(user_to_unfollow)
+    return Response({"message": "Unfollowed user."})
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, permissions
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-
+from django.contrib.auth import get_user_model
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
@@ -17,3 +17,14 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+User = get_user_model()
+
+class FeedView(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        followed_users = user.following.all()
+        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
