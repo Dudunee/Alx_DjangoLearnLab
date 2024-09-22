e27e856d7172bb463319f4f1489fbc2e5535b59c
Yahya Bakhouch <yahyabakhouchh@gmail.com>
2024-09-22 23:11:29 +0100

    social media api

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -5,8 +5,11 @@ from .views import PostViewSet, CommentViewSet, FeedView
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
+router.register(r'feed', FeedView, basename='user-feed')
 
 urlpatterns = [
     path('feed/', FeedView.as_view(), name='user-feed'),
     path('', include(router.urls)),
+    path('posts/<int:pk>/unlike/', unlike_post, name='unlike-post'),
+    path('posts/<int:pk>/like/', like_post, name='like-post'),
 ]
