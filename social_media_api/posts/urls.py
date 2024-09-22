3a2cdb12c68160707347a9aef3ed2c70aac06a0b
Yahya Bakhouch <yahyabakhouchh@gmail.com>
2024-09-22 21:53:56 +0100

    social media api

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path, include
+from django.urls import path, include 
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet, FeedView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
-router.register(r'feed', UserFeedView, basename='user-feed')
+
 urlpatterns = [
+    path('feed/', FeedView.as_view(), name='user-feed'),
     path('', include(router.urls)),
 ]
